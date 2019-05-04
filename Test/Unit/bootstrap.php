111f20306a1c50dd7d5101ede72f1e78e847f67b
A.Treitjak <a.treitjak@gmail.com>
2019-05-04 20:46:41 +0300

    Bootstrap updated.

--- Test/Unit/bootstrap.php
+++ Test/Unit/bootstrap.php
@@ -6,11 +6,3 @@
 // @codingStandardsIgnoreFile
 
 require_once realpath(__DIR__ . '/../../vendor/autoload.php');
-
-/**
- * @SuppressWarnings(PHPMD.ShortMethodName)
- */
-function __()
-{
-    return $argc = func_get_args();
-}
