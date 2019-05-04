70beb47c0d49f32588fff96997bb518a60d26e2b
A.Treitjak <a.treitjak@gmail.com>
2019-05-04 20:41:56 +0300

    Configs updated. Bootstrap added.

---
+++ Test/Unit/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  @package BelVG AWS Sqs.
+ *  @copyright 2018
+ */
+// @codingStandardsIgnoreFile
+
+require_once realpath(__DIR__ . '/../../vendor/autoload.php');
+
+/**
+ * @SuppressWarnings(PHPMD.ShortMethodName)
+ */
+function __()
+{
+    return $argc = func_get_args();
+}
